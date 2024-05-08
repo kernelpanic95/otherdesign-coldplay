d4bd7f21b2b0406a44ebda4fc32d013b3a48e9e1
Marco Cardone <marcocardone29@gmail.com>
2024-05-08 09:53:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 var quotes = [
-
-    'Svaki dan je prilika za rast',
-    'Kritika gradi tvog umjetničkog duha',
-    'Tvoj razvoj je tvoja snaga',
-    'Izazovi te oblikuju kao umjetnika',
-    'Kroz kritiku postaješ jači',
-    'Rast je put ka tvojoj najboljoj verziji',
-    'Svaki korak je prilika za učenje',
-    'Neprestano stvaraj',
-    'Kritika nije prepreka, već stepenica',
-    'Tvoja umjetnost raste s tobom',
-    'Svaki izazov je prilika za tvoj napredak',
-    'Kritika je samo još jedan korak ka tvom uspjehu',
-    'Tvoja upornost je tvoja snaga u svakom trenutku',
-    'Nikad ne prestaj sanjati',
-    'Nastavi rasti i neustrašivo stvaraj',
-    'Kroz izazove postaješ nezaustavljiv',
-    'Stvaranje je tvoja sloboda izražavanja',
-    'Vremenom otkrivaš svoju dublju svrhu',
-    'Rast je tvoj stalni saputnik',
-    'Kroz izazove cvjeta tvoja umjetnost',
-    'Kritika te čini otpornijim',
-    'Svaki dan donosi priliku za napredak',
-    'Nikad ne prestaj rasti kao umjetnik',
-    'Kritika je prilika za poboljšanje',
-    'Tvoj rast je tvoj uspjeh',
-    'Nikad ne prestaj razvijati svoju umjetnost'
+    'Lights will guide you home, and I will try..to fix you',
 ]
 
 function newQuote(){
